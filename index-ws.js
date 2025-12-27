13a7934ed36561f9d1972227c064178594e5a7bb
Yashwanth reddy <yash868686reddy@gmail.com>
2025-12-27 13:55:37 +0530

    Added database

--- index-ws.js
+++ index-ws.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
 
 server.on('request', app);
 
+process.on('SIGINT', () => {
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    });
+});
+
 server.listen(PORT, function () { console.log('Listening on ' + PORT); });
 
 /** Websocket **/
@@ -18,34 +27,55 @@ const wss = new WebSocketServer({ server: server });
 
 wss.on('connection', function connection(ws) {
     const numClients = wss.clients.size;
+    console.log('Clients connected', numClients);
 
-    console.log('clients connected: ', numClients);
-
-    wss.broadcast(`Current visitors are: ${numClients}`);
+    wss.broadcast(`Current visitors: ${numClients}`);
 
     if (ws.readyState === ws.OPEN) {
-        ws.send('welcome!');
+        ws.send('Welcome to my server');
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close() {
-        wss.broadcast(`Current visitors: ${wss.clients.size}`);
+        wss.broadcast(`Current visitors: ${numClients}`);
         console.log('A client has disconnected');
     });
 
-    ws.on('error', function error() {
-        //
-    });
 });
 
-/**
- * Broadcast data to all connected clients
- * @param  {Object} data
- * @void
- */
 wss.broadcast = function broadcast(data) {
-    console.log('Broadcasting: ', data);
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
-};
-/** End Websocket **/
+}
+
+/** end websockets */
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    console.log('Shutting down db');
+
+    getCounts();
+    db.close();
+
+}
